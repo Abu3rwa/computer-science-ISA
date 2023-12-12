30b833bc823b38ce1eea69d03905d71ec18d81be
abu3rwa <abu3rwa.abdulhafeez@gmail.com>
2023-12-13 02:37:44 +0300

    bubble sort

--- 2-algorithms/1-sorting/bubble_sort.py
+++ 2-algorithms/1-sorting/bubble_sort.py
@@ -1,13 +1,21 @@
-def bubble_sort(arr):
+
+def bubble_sort():
+    arr = [1,3,0,75,4,7,8]
     sorted = False
     while not sorted:
         sorted = True
         for i in range(len(arr)-1):
+            print(arr[i], arr[i+1])
             if arr[i]>arr[i+1]:
-                sorted = False
-                arr[i],arr[i+1] = arr[i+1],arr[i]
+                # arr[i],arr[i+1] = arr[i+1],arr[i]
+                # temp = arr[i]
+                # arr[i] = arr[i+1]
+                # arr[1+1] =  temp
+                sorted = False    
     print(arr)      
     return arr  
 
+bubble_sort()
 
-bubbleSort([1,3,5,75,4,7,8])
+
+
---
+++ robotics/file.py
@@ -0,0 +1,9 @@
+# def printName(student): #parameter
+#     print(student)
+
+# printName("Fatima") # arguement
+# name = "Ali" # initialization : giving the variable a value when you make it
+# student = '' # declaration : not giving the variable a value when you make it
+# age = 0 # declaration : not giving the variable a value when you make it
+# student = "Khalid"
+# print(student)
